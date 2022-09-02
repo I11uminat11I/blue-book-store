c59da87f0e872b9aad663960824b7b3b681d5b2c
I11umiat11I <a.kostarev.nytva@yandex.ru>
2022-09-03 00:13:44 +0500

    fixed columns and category slug

--- ecommerce/apps/catalogue/views.py
+++ ecommerce/apps/catalogue/views.py
@@ -17,5 +17,5 @@ def product_detail(request, slug):
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category__in=Category.objects.get(name=category_slug).get_descendants(include_self=True))
+    products = Product.objects.filter(category__in=Category.objects.get(slug=category_slug).get_descendants(include_self=True))
     return render(request, "catalogue/category.html", {"category": category, "products": products})
